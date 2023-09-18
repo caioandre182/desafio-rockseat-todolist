66bf3a924a21e0ea892ca4afb41f31d39e65e3f3
caioandre182 <caioserralvo182@gmail.com>
2023-09-17 21:24:33 -0300

    feat:criação da estrutura do projeto

--- react-todo-list/src/App.tsx
+++ react-todo-list/src/App.tsx
@@ -2,13 +2,14 @@ import './global.css';
 
 import { Header } from './components/Header';
 import { NewTask } from './components/NewTask';
+import { BoardTask } from './components/BoardTask';
 
 export function App(){
     return (
         <div>
             <Header />
             <NewTask />
-            
+            <BoardTask />
         </div>
     )
 }
